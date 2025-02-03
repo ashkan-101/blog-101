a0a12f360394b34677c1b1710367784d417d576d
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-03 20:19:02 +0330

    init typeOrm config in module/app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from 'src/common/configs/typeOrmConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule, AuthModule,
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
 })
 export class AppModule {}
