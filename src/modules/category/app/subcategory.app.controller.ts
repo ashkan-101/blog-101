d9b1819959d7d8076db45a914516660ec5c5babd
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-05 22:17:49 +0330

    added api for get subcategories in modules/category/app/subcategory

--- src/modules/category/app/subcategory.app.controller.ts
+++ src/modules/category/app/subcategory.app.controller.ts
@@ -1,11 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, DefaultValuePipe, Get, ParseIntPipe, Query } from "@nestjs/common";
 import { SubcategoryAppService } from "./subcategory.app.service";
 
 
-@Controller('/api/v1/subcategory')
+@Controller('/api/v1/subcategories')
 export class SubcategoryAppController {
   constructor(
     private readonly subcategoryAppService: SubcategoryAppService
   ){}
   
+  @Get()
+  async getSubcategories(@Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number){
+    const {totalPages, subcategories} = await this.subcategoryAppService.findAllSubcategories(page)
+
+    return {
+      totalPages,
+      subcategories
+    }
+  }
 }
--- src/modules/category/app/subcategory.app.service.ts
+++ src/modules/category/app/subcategory.app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { SubcategoryEntity } from "../entities/subcategory.entity";
+import { paginateTool } from "src/common/utils/pagination";
 
 @Injectable()
 export class SubcategoryAppService {
@@ -9,4 +10,26 @@ export class SubcategoryAppService {
     @InjectRepository(SubcategoryEntity)
     private readonly subcategoryRepository: Repository<SubcategoryEntity>
   ){}
+
+  //-------------------------------------private methods
+
+
+
+  //-------------------------------------public methods
+
+  public async findAllSubcategories(page: number): Promise<{totalPages: number, subcategories: SubcategoryEntity[]}>{
+    const pagination = paginateTool({page, take: 20})
+
+    const [subcategories, totalCount] = await this.subcategoryRepository.findAndCount({
+      order: {createdAt: 'DESC'},
+      take: pagination.take, skip: pagination.skip
+    })
+
+    return {
+      totalPages: Math.ceil(totalCount / pagination.take),
+      subcategories
+    }
+  }
+
+  //-------------------------------------export methods
 }
