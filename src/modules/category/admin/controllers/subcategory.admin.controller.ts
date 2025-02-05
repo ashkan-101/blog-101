299df5860211db6098aa66c102ff1d02be2eb1ac
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-05 17:03:11 +0330

    added swagger document for controller newSubcategory in modules/category/admin/subcategoryController

--- src/modules/category/admin/controllers/subcategory.admin.controller.ts
+++ src/modules/category/admin/controllers/subcategory.admin.controller.ts
@@ -4,6 +4,7 @@ import { NewSubcategoryDto } from "../dtos/subcategory/new-subcategory.dto";
 import { JwtGuard } from "src/modules/auth/guards/jwt.guard";
 import { RoleGuard } from "src/common/guards/role.guard";
 import { SetAccessRoles } from "src/common/decorators/SetAccessRoles";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 
 @Controller('/api/v1/subcategory-admin')
@@ -12,6 +13,30 @@ export class SubcategoryAdminController {
     private readonly subcategoryAdminService: SubcategoryAdminService
   ) {}
 
+  @ApiOperation({ summary: 'Create a new subcategory' })
+  @ApiBody({
+    description: 'The data required to create a new subcategory',
+    type: NewSubcategoryDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Subcategory created successfully',
+    schema: {
+      example: {
+        id: 'sub-category-id',
+        title: 'Subcategory Title',
+        categoryId: 'category-id',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Title already exists for a subcategory',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Category not found with the given ID',
+  })
   @UseGuards(JwtGuard, RoleGuard)
   @SetAccessRoles(['admin', 'superadmin'])
   @Post('/create')
