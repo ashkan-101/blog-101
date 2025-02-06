fa03198ac90f7247daaea65c1b7761e1b73bca11
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-06 19:34:35 +0330

    added swagger document for api createAdmin and create-admin.dto

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { CreateAdminDto } from "./dtos/create-admin.dto";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 
 @Controller('/api/v1/admins')
@@ -9,6 +10,42 @@ export class AdminController {
     private readonly adminService: AdminService
   ){}
 
+
+
+  @ApiOperation({ summary: 'Create new admin' })
+  @ApiBody({
+    description: 'Details of the new admin to be created',
+    type: CreateAdminDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Admin created successfully.',
+    schema: {
+      type: 'object',
+      properties: {
+        userName: { type: 'string', example: 'adminUser123' },
+        email: { type: 'string', example: 'admin@example.com' },
+        avatar: { type: 'string', example: 'https://avatar.url/image.jpg' },
+        role: { type: 'string', example: 'admin' },
+        isActive: { type: 'boolean', example: true },
+        createdAt: { type: 'string', format: 'date-time', example: '2025-02-06T10:00:00Z' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request due to validation failure (username or email already exists).',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request due to password mismatch.',
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', example: 'The repeated password does not match the entered password' },
+      },
+    },
+  })
   @Post()
   async createAdmin(@Body() body: CreateAdminDto){
     const newAdmin = await this.adminService.createNewAdmin(body)
--- src/modules/admin/dtos/create-admin.dto.ts
+++ src/modules/admin/dtos/create-admin.dto.ts
@@ -1,22 +1,31 @@
-import { IsString, IsEmail, IsStrongPassword, Length } from 'class-validator'
+import { IsString, IsEmail, IsStrongPassword, Length, IsNotEmpty } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateAdminDto {
 
+  @IsNotEmpty()
   @IsString()
   @Length(5,20)
+  @ApiProperty({type: String, required: true, description: 'set userName for admin'})
   userName: string
 
+  @IsNotEmpty()
   @IsString()
   @IsEmail()
+  @ApiProperty({type: String, required: true, description: 'unique email create admin'})
   email: string
 
+  @IsNotEmpty()
   @IsString()
   @Length(8, 20)
   // @IsStrongPassword()
+  @ApiProperty({type: String, required: true, description: 'set strong password for admin account'})
   password: string
 
+  @IsNotEmpty()
   @IsString()
   @Length(8, 20)
+  @ApiProperty({type: String, required: true, description: 'repeqt password for admin'})
   // @IsStrongPassword()
   repeatPassword: string
 }
