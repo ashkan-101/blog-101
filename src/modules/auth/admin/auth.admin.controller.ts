4ce58e390730e5fd4930c9236dfea63ea78092f3
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-07 15:09:56 +0330

    added swagger document for api signin admin in module/auth/admin/controller

--- src/modules/auth/admin/auth.admin.controller.ts
+++ src/modules/auth/admin/auth.admin.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthAdminService } from "./auth.admin.service";
 import { SignInAdminDto } from "./dtos/signIn-admin.dto";
 import { JwtService } from "@nestjs/jwt";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 
 @Controller('/api/v1/auth')
@@ -11,6 +12,31 @@ export class AuthAdminController{
 
   ){}
 
+  @ApiOperation({
+    summary: 'Admin Sign-In',
+    description: 'Authenticates an admin by email and password, and returns a JWT token if credentials are correct.',
+  })
+  @ApiBody({
+    description: 'Sign-In Admin Credentials',
+    type: SignInAdminDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Admin signed in successfully, returns JWT token.',
+    schema: {
+      example: {
+        token: 'your-jwt-token-here',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid credentials, either email or password is incorrect.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'not found admin with entered email',
+  })
   @Post('/signin-admin')
   async signIn(@Body() body: SignInAdminDto){
     const jwt = await this.authAdminService.authenticateAdmin(body)
