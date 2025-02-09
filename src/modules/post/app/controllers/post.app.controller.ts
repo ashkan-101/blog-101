b0f7c511783cc21d1d87c8e89f69ccb90949d499
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-09 17:33:53 +0330

    added api for get all posts with sorting and pagination in modules/post/app/{controller & service}

--- src/modules/post/app/controllers/post.app.controller.ts
+++ src/modules/post/app/controllers/post.app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller } from "@nestjs/common";
+import { Controller, DefaultValuePipe, Get, ParseIntPipe, Query, UseGuards } from "@nestjs/common";
 import { PostAppService } from "../services/post.app.service";
+import { PostSorting } from "../../enums/Post.Sorting";
+import { JwtAppGuard } from "src/modules/auth/guards/jwt.app.guard";
 
 
 @Controller()
@@ -8,5 +10,16 @@ export class PostAppController {
     private readonly postAppService: PostAppService
   ){}
 
-  
+  @UseGuards(JwtAppGuard)
+  @Get()
+  async getPosts(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('sortingBy', new DefaultValuePipe(PostSorting.POPULAR)) sortingBy: PostSorting
+  ){
+    const {totalPages, posts }= await this.postAppService.getAllPosts(page, sortingBy)
+    return { 
+      totalPages,
+      posts
+     }
+  }
 }
--- src/modules/post/app/services/post.app.service.ts
+++ src/modules/post/app/services/post.app.service.ts
@@ -4,6 +4,8 @@ import { PostEntity } from "../../entities/post.entity";
 import { Repository } from "typeorm";
 import { UserEntity } from "src/modules/user/entities/user.entity";
 import { PostAppFactory } from "../post.app.factory";
+import { paginateTool } from "src/common/utils/paginate.tool";
+import { PostSorting } from "../../enums/Post.Sorting";
 
 
 @Injectable()
@@ -13,4 +15,32 @@ export class PostAppService {
     private readonly postRepository: Repository<UserEntity>,
     private readonly postAppFactory: PostAppFactory
   ){}
+
+  //--------------------------------private methods
+
+
+
+  //--------------------------------public methods
+
+  public async getAllPosts(page: number, sorting: PostSorting){
+    const pagination = paginateTool({page, take: 20})
+  
+    const queryBuilder = this.postRepository.createQueryBuilder('post');
+  
+    if(sorting === PostSorting.NEWEST) queryBuilder.orderBy('post.createdAt', 'DESC')
+    if(sorting === PostSorting.POPULAR) queryBuilder.orderBy('post.views', 'DESC')
+  
+    const [posts, totalCount] = await queryBuilder
+      .leftJoinAndSelect('post.author', 'author')
+      .leftJoinAndSelect('post.subcategory', 'subcategory')
+      .leftJoinAndSelect('subcategory.category', 'category')
+      .skip(pagination.skip)
+      .take(pagination.take)
+      .getManyAndCount()
+  
+      return {
+        totalPages: Math.ceil(totalCount / pagination.take),
+        posts
+      }
+  }
 }
