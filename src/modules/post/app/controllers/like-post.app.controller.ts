eace1cc900686190483a8d308d0489196f9b9dee
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-10 15:12:15 +0330

    added swagger documents for apis post-like in modules/post/app/controllers/like-post

--- src/modules/post/app/controllers/like-post.app.controller.ts
+++ src/modules/post/app/controllers/like-post.app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param, ParseUUIDPipe, Patch, Req, UseGuards } from "@nestjs/common";
 import { LikePostAppService } from "../services/like-post.app.service";
-import { JwtAdminGuard } from "src/modules/auth/guards/jwt.admin.guard";
 import { JwtAppGuard } from "src/modules/auth/guards/jwt.app.guard";
+import { ApiOperation, ApiParam, ApiResponse } from "@nestjs/swagger";
 
 @Controller('/api/v1/posts/like')
 export class LikePostAppController {
@@ -9,6 +9,21 @@ export class LikePostAppController {
     private readonly likeAppService: LikePostAppService
   ){}
 
+  @ApiOperation({ summary: 'Like or Dislike a post by user' })
+  @ApiParam({ name: 'postId', description: 'The UUID of the post to like or dislike' })
+  @ApiResponse({
+    status: 200,
+    description: 'Like or Dislike the post',
+    schema: {
+      example: {
+        likeResult: 'like or dislike',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Post not found with the provided ID',
+  })
   @UseGuards(JwtAppGuard)
   @Patch(':postId')
   async like(@Param('postId', ParseUUIDPipe) postId: string, @Req() req){
@@ -18,6 +33,29 @@ export class LikePostAppController {
     return { likeResult }
   }
 
+  @ApiOperation({ summary: 'Get the likes for a specific post' })
+  @ApiParam({
+    name: 'postId',
+    description: 'The UUID of the post to get the likes for',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the likes of the post along with the current user ID',
+    schema: {
+      example: {
+        likes: [
+          { id: 'likeId1', user: { id: 'userId1' }},
+          { id: 'likeId2', user: { id: 'userId2' }},
+        ],
+        likesCount: 2,
+        currentUser: 'currentUserId',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Post not found with the provided ID',
+  })
   @UseGuards(JwtAppGuard)
   @Get(':postId')
   async getPostLikes(@Param('postId', ParseUUIDPipe) postId: string, @Req() req){
