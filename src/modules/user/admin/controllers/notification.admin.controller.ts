ca7e0524047f9da46c0a23bd248d166fa17aa6fa
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-10 19:01:15 +0330

    added swagger document for api create new notification in modules/user/admin/controllers/notification

--- src/modules/user/admin/controllers/notification.admin.controller.ts
+++ src/modules/user/admin/controllers/notification.admin.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Param, ParseUUIDPipe, Post, UseGuards } from "@nestjs
 import { NotificationAdminService } from "../services/notification.admin.service";
 import { CreateNotificationDto } from "../dtos/create-notification.dto";
 import { JwtAdminGuard } from "src/modules/auth/guards/jwt.admin.guard";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from "@nestjs/swagger";
 
 
 @Controller('/api/v1/admin/notifications')
@@ -10,7 +11,29 @@ export class NotificationAdminController {
     private readonly notificationAdminService: NotificationAdminService
   ){}
 
-  // @UseGuards(JwtAdminGuard)
+  @ApiOperation({ 
+    summary: 'Send notification to a specific user',
+    description: 'This endpoint allows an admin to send a notification to a user by their ID.'
+  })
+  @ApiParam({
+    name: 'userId',
+    description: 'The ID of the user to whom the notification will be sent.',
+    type: String,
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @ApiBody({
+    description: 'Notification details that will be sent to the user',
+    type: CreateNotificationDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Notification created successfully',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+  })
+  @UseGuards(JwtAdminGuard)
   @Post(':userId')
   async sendForUser(@Param('userId', ParseUUIDPipe) userId: string, @Body() body: CreateNotificationDto){
     const newNotification = await this.notificationAdminService.createNotificationForUser(userId, body)
