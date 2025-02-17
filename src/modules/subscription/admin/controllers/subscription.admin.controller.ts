fa18e4fe96868a06235eacc85affdee7fc68d05d
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-17 22:48:23 +0330

    added swagger document for api create new subscription by admin in modules/subscription/admin/controller

--- src/modules/subscription/admin/controllers/subscription.admin.controller.ts
+++ src/modules/subscription/admin/controllers/subscription.admin.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { SubscriptionAdminService } from "../services/subscription.admin.service";
 import { CreateSubscriptionDto } from "../dtos/create-subscription.dto";
 import { JwtAdminGuard } from "src/modules/auth/guards/jwt.admin.guard";
-
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 @Controller('/api/v1/admin/subscription')
 export class SubscriptionAdminController {
@@ -10,6 +10,32 @@ export class SubscriptionAdminController {
     private readonly subscriptionAdminService: SubscriptionAdminService
   ){}
 
+  @ApiOperation({
+    summary: 'Create a new subscription plan by ADMIN',
+    description: 'This endpoint is used to create a new subscription plan in the system.',
+  })
+  @ApiBody({
+    description: 'The data required to create a new subscription plan.',
+    type: CreateSubscriptionDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The new subscription plan has been successfully created.',
+    schema: {
+      example: {
+        newSubscription: {
+          id: 1,
+          name: 'Premium Plan',
+          price: 50,
+          duration: 30,
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. The provided data is invalid.',
+  })
   @UseGuards(JwtAdminGuard)
   @Post()
   async create(@Body() body: CreateSubscriptionDto){
