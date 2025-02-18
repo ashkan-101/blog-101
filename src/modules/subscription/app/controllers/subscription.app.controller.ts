38f0840171db3b9399fadbbaf4dbfaf6033135fc
ashkan-101 <ashkan.dev.101@gmail.com>
2025-02-18 18:06:09 +0330

    added swagger document for api get all subscriptions in modules/subscriptions/app/subscription-controller

--- src/modules/subscription/app/controllers/subscription.app.controller.ts
+++ src/modules/subscription/app/controllers/subscription.app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { SubscriptionAppService } from "../services/subscription.app.service";
 import { JwtAppGuard } from "src/modules/auth/guards/jwt.app.guard";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 
 @Controller('/api/v1/subscription')
@@ -9,6 +10,32 @@ export class SubscriptionAppController{
     private readonly subscriptionAppService: SubscriptionAppService
   ){}
 
+  @ApiOperation({ summary: 'Get all subscriptions' }) 
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully fetched the subscription plans',
+    schema: {
+      type: 'object',
+      properties: {
+        totalPlans: {
+          type: 'integer',
+          example: 10,
+        },
+        subscriptions: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'integer', example: 1 },
+              name: { type: 'string', example: 'Basic Plan' },
+              durationDays: { type: 'integer', example: 30 },
+              price: { type: 'number', example: 10000 },
+            },
+          },
+        },
+      },
+    },
+  })
   @UseGuards(JwtAppGuard)
   @Get()
   async getSubscriptions(){
